44e0bfbadde7c516678788b5239e6961f43bba89
Marylou Groeneveld <marylougroeneveld@gmail.com>
2026-06-24 18:59:52 +0200

    create admin layout, header

---
+++ resources/views/partials/admin/header.blade.php
@@ -0,0 +1,11 @@
+<header class="bg-linear-to-t from-pink-500 to-pink-400 text-pink-100 flex justify-around p-5">
+    <h1 class="bg-linear-to-t from-pink-50 to-pink-300 bg-clip-text text-transparent text-5xl font-bold">
+        SoftWire Admin Dashboard
+    </h1>
+
+    <a href="/"
+        class="underline">
+        Back to main website
+    </a>
+
+</header>
