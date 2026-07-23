f7b0f75f0887c378c3a84f0aacf5ad640a1a71e5
Marylou Groeneveld <marylougroeneveld@gmail.com>
2026-07-23 20:56:51 +0200

    add inactive state, tooltip for nav elements

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -1,7 +1,7 @@
 <nav class="text-pink-900 text-center underline flex gap-3 justify-center backdrop-blur-lg divide-x-2 divide-pink-900">
             <a href="/" class="px-2">Home</a>
-            <a href="#" class="px-2">Popular</a>
-            <a href="#" class="px-2">FAQ</a>
-            <a href="#" class="px-2">Contact</a>
-            <a href="#" class="px-2">Refer a friend</a>
+            <a href="#" class="px-2 inactive" title="Coming soon!">Popular</a>
+            <a href="#" class="px-2 inactive" title="Coming soon!">FAQ</a>
+            <a href="#" class="px-2 inactive" title="Coming soon!">Contact</a>
+            <a href="#" class="px-2 inactive" title="Coming soon!">Refer a friend</a>
         </nav>
