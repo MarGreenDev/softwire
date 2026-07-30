5efbdf93e40511c99aef100357a2b7969904b689
Marylou Groeneveld <marylougroeneveld@gmail.com>
2026-07-30 19:53:22 +0200

    create favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="{{ asset('images/icons/favicon.ico') }}">
     <title>@yield('title', 'SoftWire')</title>
 
     @vite(['resources/css/app.css', 'resources/js/app.ts'])
