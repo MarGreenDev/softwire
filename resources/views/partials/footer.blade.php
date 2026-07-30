1ebe1f3852098cdc45df0a6899cf97b95267f5b5
Marylou Groeneveld <marylougroeneveld@gmail.com>
2026-07-30 19:59:56 +0200

    update footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,3 @@
 <footer class="bg-pink-400/70 text-white text-center max-w-5xl w-full mx-auto">
-    © MarGreenDev 2026
+    <span> © SoftWire 2026 • Marylou Groeneveld (MarGreenDev) </span>
 </footer>
