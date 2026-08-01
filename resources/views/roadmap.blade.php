2889dfa29cea126b72b5e3ae3ef093a19e4ba77b
Marylou Groeneveld <marylougroeneveld@gmail.com>
2026-08-01 13:35:56 +0200

    fix roadmap responsiveness issue

--- resources/views/roadmap.blade.php
+++ resources/views/roadmap.blade.php
@@ -4,63 +4,66 @@
 
 @section('content')
 
-<div class="bg-pink-100/70 backdrop-blur-sm col-span-12 flex-1">
+<div class="bg-pink-100/70 backdrop-blur-sm col-span-1 md:col-span-12 flex-1">
 
     <div class="flex flex-col p-5 items-center text-pink-600">
 
         <article class="flex flex-col gap-5">
-            <div class="text-center">
+            <div class="w-full text-center">
                 <h2 class="font-bold text-4xl text-pink-900">✰SoftWire Roadmap✰</h2>
                 <span class="opacity-60">Last updated: July 2026</span>
             </div>
-            <div class="w-xl p-4 flex flex-col gap-3">
+            <div class="w-full max-w-xl p-4 flex flex-col gap-3">
                 <p>Welcome! Thanks for checking out the roadmap!</p>
                 <p>SoftWire is a passion project that I work on in my free time. This page gives you a look at what I'm working on and plans I have for the
                     future. Plans may change!
                 </p>
             </div>
 
-            <section>
-                <!--  currently working on-->
-                <h3 class="font-semibold text-3xl text-pink-800">Currently working on</h3>
+            <div class="flex flex-col p-4 gap-5">
 
-                <ul class="list-disc">
-                    <li>Posts</li>
-                    <li>Friend system</li>
-                    <li>Improving the UI</li>
-                    <li>Fixing bugs</li>
-                </ul>
-            </section>
+                <section>
+                    <!--  currently working on-->
+                    <h3 class="font-semibold text-3xl text-pink-800">Current goals</h3>
 
-            <section>
-                <!-- future plans -->
-                <h3 class="font-semibold text-3xl text-pink-800">Future plans</h3>
+                    <ul class="list-disc">
+                        <li>Posts</li>
+                        <li>Friend system</li>
+                        <li>Improving the UI</li>
+                        <li>Fixing bugs</li>
+                    </ul>
+                </section>
 
-                <ul class="list-disc">
-                    <li>Other colour themes</li>
-                    <li>More profile widgets</li>
-                    <li>Custom stickers/emoticons</li>
-                    <li>Private messaging</li>
-                    <li>Markdown support</li>
-                    <li>... And much more!</li>
-                </ul>
+                <section>
+                    <!-- future plans -->
+                    <h3 class="font-semibold text-3xl text-pink-800">Future plans</h3>
+
+                    <ul class="list-disc">
+                        <li>Other colour themes</li>
+                        <li>More profile widgets</li>
+                        <li>Custom stickers/emoticons</li>
+                        <li>Private messaging</li>
+                        <li>Markdown support</li>
+                        <li>... And much more!</li>
+                    </ul>
 
 
-            </section>
+                </section>
 
-            <section class="flex flex-col gap-3">
-                <!-- feedback appreciated -->
-                <h3 class="font-semibold text-3xl text-pink-800">Feedback welcome!</h3>
+                <section class="flex flex-col gap-3">
+                    <!-- feedback appreciated -->
+                    <h3 class="font-semibold text-3xl text-pink-800">Feedback welcome!</h3>
 
-                <p>Have an idea or found a bug?</p>
-                <p>I'd love to hear your feedback! Feel free to contact me by sending me an email or a dm through Discord!</p>
+                    <p>Have an idea or found a bug?</p>
+                    <p>I'd love to hear your feedback! Feel free to contact me by sending me an email or a dm through Discord!</p>
 
-                <ul class="list-disc">
-                    <li>📧E-mail: margreendev@gmail.com</li>
-                    <li>👾Discord user: marymastertje</li>
-                </ul>
-            </section>
+                    <ul class="list-disc">
+                        <li>📧E-mail: margreendev@gmail.com</li>
+                        <li>👾Discord user: marymastertje</li>
+                    </ul>
+                </section>
 
+            </div>
         </article>
 
 
